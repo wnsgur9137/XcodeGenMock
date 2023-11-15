dd438b69d2430ef40f5b0af89d08cbb011bd2dfd
JunHyeok0206 <jh.lee@fanmaum.com>
2023-11-15 09:44:24 +0900

    XcodeGen

---
+++ XcodeGenMock/Presentation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XcodeGenMock
+//
+//  Created by JunHyeok Lee on 11/15/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ readme.md
@@ -0,0 +1,28 @@
+## Blog
+
+https://medium.com/@junhyeok9137/swift-xcodegen-0c258f380d36
+
+<br>
+
+## 설치
+
+```
+brew install xcodegen
+```
+
+<br>
+
+## .gitignore
+
+```
+*.xcodeproj
+*.xcworkspace
+```
+
+<br>
+
+## 실행
+
+``` 
+xcodegen generate
+```
